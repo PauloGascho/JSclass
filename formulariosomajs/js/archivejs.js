39aeea785b9ac371ea6a382829ded4a373f73c9a
PauloGascho <prgascho@gmail.com>
2019-11-29 17:54:45 -0300

    include starting files in js

---
+++ formulariosomajs/js/archivejs.js
@@ -0,0 +1,10 @@
+function enviar(){
+
+var var1 = document.getElementById("v1").value;
+var var2 = document.getElementById("v2").value;
+
+var var3 = parseInt(var1) + parseInt(var2);
+
+alert(var3);
+
+}
---
+++ segundojs/action.js
@@ -0,0 +1,4 @@
+<importScripts></importScripts>
+
+let han = new Person('Han', 'Solo', 25, 'male', ['Smuggling']);
+han.greeting();
