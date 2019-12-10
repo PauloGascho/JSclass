f35e01c90f7022f5d0f28be07fbbc2f2dff237cf
PauloGascho <prgascho@gmail.com>
2019-12-10 06:01:09 -0300

    adicionando arquivo sobre variáveis boolean types com !!

---
+++ formulariosomajs/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+    
+    <script
+        type="text/javascript"
+        src="js/archivejs.js">
+    </script>
+ <h1> Teste </h1>
+  <?
+    include_once("php/archivehtml.html")
+?>
+</html>
