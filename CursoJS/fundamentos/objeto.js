3e5c409d343b33ef8fc8d873b243053c422148f5
PauloGascho <prgascho@gmail.com>
2019-12-10 08:19:55 -0300

    adicionando arquivo sobre arrays e sobre objeto

---
+++ CursoJS/fundamentos/objeto.js
@@ -0,0 +1,26 @@
+//OBJETO em JS é um conjunto de pares (chave e valor)
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+// { nome: 'Celular Ultra Mega' }
+// chave: nome; valor:
+
+//atribuindo valor dinamicamente
+prod1.preco = 4980.90
+
+prod1["Desconto legal"] = 0.40 //evitar atributos com espaço
+
+console.log(prod1)
+
+//Criando objeto de forma construtora
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 19.50,
+}
+
+//é possível aninhar objetos
+const obj1 {
+    atributo: 
+    obj2: {
+        atributo: 'um atributo',
+    }
+}
