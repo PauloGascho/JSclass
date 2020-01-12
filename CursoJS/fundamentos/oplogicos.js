8ee2704f2420668d2f615fe9aa84f41ba6189dc4
PauloGascho <prgascho@gmail.com>
2020-01-12 15:21:12 -0200

    revisão até pararâmetros variáveis

--- CursoJS/fundamentos/oplogicos.js
+++ CursoJS/fundamentos/oplogicos.js
@@ -5,7 +5,13 @@ function compras(trab1, trab2){
     const compTV32 = trab1 != trab2
     const mantSaud = !compSorv //operador unário
 
-    return { compSorv, compTV50, compTV32, mantSaud }
+//JS só suporta um return, mas é possível retornar tudo em um objeto
+    /*
+    return { compSorv: compSorv, compTV50: compTV50, compTV32:compTV32, mantSaud: mantSaud } 
+}
+    */
+//Simplificando o obj (recurso novo do ES)
+    return { compSorv, compTV50, compTV32, mantSaud } //JS só suporta um return, mas é possível retornar tudo em um objeto
 }
 
 console.log(compras(true, true))
--- CursoJS/fundamentos/opunarios.js
+++ CursoJS/fundamentos/opunarios.js
@@ -4,5 +4,5 @@ let num2 = 2
 num1++
 console.log(num1)
 --num1
-console.log(++num1 === num2--)
-console.log(num1 === num2)
+console.log(++num1 === num2--) // O num2-- será executado depois da compração, retornandum uma expressão verdadeira (2=2)
+console.log(num1 === num2) // Desta vez retornará falso pois o num2-- foi executado
