deb1f214842774a3c85c6da2140f5f1e7be346ba
PauloGascho <prgascho@gmail.com>
2020-01-28 06:10:39 -0300

    Operador Spread/Rest e tagged templates

---
+++ esnext/taggedTemplate2.js
@@ -0,0 +1,7 @@
+function real(partes, ...valores) {
+    const resultado = []
+    valores.forEach((valor, indice) => {
+        valor = isNaN(valor) ? valor : `R$${valor.tofixed(2)}`
+        resultado.push()
+    })
+}
