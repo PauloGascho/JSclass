4a7b805233b43a34bcc9994369f863d4b50ca65c
PauloGascho <prgascho@gmail.com>
2020-01-30 06:06:35 -0300

    map corrigido e set no ESNext

--- esnext/map.js
+++ esnext/map.js
@@ -5,10 +5,11 @@ tecnologias.set('angular', { framework: true })
 console.log(tecnologias.react)
 console.log(tecnologias.get('react').framework)
 
+
 const chavesVariadas = new Map([
     [function () { }, 'Função'],
-    [{}, 'Objeto']
-    [123, 'Número']
+    [{}, 'Objeto'],
+    [123, 'Número'],
 ])
 
 chavesVariadas.forEach((vl, ch) => {
@@ -19,3 +20,9 @@ console.log(chavesVariadas.has(123))
 chavesVariadas.delete(123)
 console.log(chavesVariadas.has(123))
 console.log(chavesVariadas.size)
+
+chavesVariadas.set(123, 'a')
+chavesVariadas.set(123, 'b')
+chavesVariadas.set(456, 'b')
+
+console.log(chavesVariadas)
